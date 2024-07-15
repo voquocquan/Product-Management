0b31706e7116caaf2ee826773974287f797c3cc6
Võ Quốc Quân <vo.qquan@gmail.com>
2024-07-16 06:36:49 +0700

    Tạo mới sản phẩm (Preview hình ảnh)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -162,5 +162,23 @@ if (showAlert) {
         showAlert.classList.add("alert-hidden");
     })
 }
-
 //End Show alert
+
+
+//Upload Image
+const uploadImage = document.querySelector("[upload-image]")
+if (uploadImage) {
+    const uploadImageInput = document.querySelector("[upload-image-input]");
+    const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+    uploadImageInput.addEventListener("change", (e) => {
+        console.log(e);
+        const file = e.target.files[0];
+        if (file) {
+            uploadImagePreview.src = URL.createObjectURL(file);
+        }
+    });
+}
+
+
+//End Upload Image
