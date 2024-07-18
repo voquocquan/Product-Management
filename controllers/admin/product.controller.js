26428be7bcaa78e43ceb65e3e5cb3226b713e1b4
Võ Quốc Quân <vo.qquan@gmail.com>
2024-07-18 08:20:32 +0700

    Đẩy file tĩnh lên Cloud

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -166,11 +166,6 @@ module.exports.createPost = async (req, res) => {
         req.body.position = parseInt(req.body.position);
     }
 
-    if (req.file) {
-        req.body.thumbnail = `/uploads/${req.file.filename}`;
-    }
-
-
     const product = new Product(req.body)
     await product.save();
 
--- routers/admin/product.route.js
+++ routers/admin/product.route.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const multer  = require('multer');
+const multer = require('multer');
+
 const router = express.Router();
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() });
+
+const upload = multer();
 
 
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product.validate");
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware")
 
 router.get("/", controller.index);
 
@@ -19,8 +22,9 @@ router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create);
 
 router.post(
-    "/create", 
+    "/create",
     upload.single('thumbnail'),
+    uploadCloud.upload,
     validate.createPost,
     controller.createPost
 );
