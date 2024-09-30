fb7bbaf0b0cab5df208cdcab4a2c03ee34091069
Võ Quốc Quân <vo.qquan@gmail.com>
2024-09-30 09:54:32 +0700

    Sắp xếp sản phẩm theo tiêu chí

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -41,8 +41,17 @@ module.exports.index = async (req, res) => {
 
     //
 
+    //Sort
+    let sort = {};
+    if(req.query.sortKey && req.query.sortValue) {
+        sort[req.query.sortKey] = req.query.sortValue;
+    } else {
+        sort.position = "desc";
+    }
+    //End Sort
+
     const products = await Product.find(find)
-        .sort({ position: "desc" })
+        .sort(sort)
         .limit(objectPagination.limitItem)
         .skip(objectPagination.skip);
 
