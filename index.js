15f7f0de2beefdf9d93c7b7836c71bda7246668a
Võ Quốc Quân <vo.qquan@gmail.com>
2024-11-28 16:48:24 +0700

    TinyMCE

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -32,6 +33,13 @@ app.set('view engine', 'pug');
 app.use(cookieParser('mykeyhihi'));
 app.use(session({ cookie: { maxAge: 60000 }}));
 app.use(flash());
+
+//TinyMCE
+app.use(
+    '/tinymce', 
+    express.static(path.join(__dirname, 'node_modules', 'tinymce'))
+);
+//End TinyMCE
 
 
 //App Locals Variable
