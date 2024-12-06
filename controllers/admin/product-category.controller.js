1b5f25e92e32c1c0dccf630aa05b918e5994c5d8
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-06 15:59:11 +0700

    update danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,6 +1,8 @@
 const ProductCategory = require("../../models/product-category.model")
 const systemConfig = require("../../config/system")
 
+const createTreeHelper = require("../../helpers/createTree")
+
 // [GET]   /admin/products-category
 module.exports.index = async (req, res) => {
     let find = {
@@ -9,16 +11,27 @@ module.exports.index = async (req, res) => {
 
     const records = await ProductCategory.find(find)
 
+    const newRecords = createTreeHelper.tree(records);
+
     res.render("admin/pages/products-category/index", {
         pageTitle: "Danh mục sản phẩm",
-        records: records,
+        records: newRecords,
     });
 }
 
 // [GET]   /admin/products-category/create
 module.exports.create = async (req, res) => {
+    let find = {
+        deleted: false,
+    };
+
+    const records = await ProductCategory.find(find)
+
+    const newRecords = createTreeHelper.tree(records);
+    
     res.render("admin/pages/products-category/create", {
         pageTitle: "Tạo danh mục sản phẩm",
+        records: newRecords,
     });
 }
 
