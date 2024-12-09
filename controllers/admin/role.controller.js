97ad043993125b8fc439188b96798329234a7802
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-09 12:31:31 +0700

    sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -3,11 +3,9 @@ const systemConfig = require("../../config/system")
 
 // [GET]   /admin/roles
 module.exports.index = async (req, res) => { 
-
     let find = {
         deleted: false
     }
-
     const records = await Role.find(find);
     res.render("admin/pages/roles/index.pug", {
         pageTitle: "Nhóm quyền",
@@ -24,9 +22,45 @@ module.exports.create = async (req, res) => {
 
 // [POST]   /admin/roles/create
 module.exports.createPost = async (req, res) => { 
-
     const record = new Role(req.body);
     await record.save();
     
     res.redirect(`${systemConfig.preFixAdmin}/roles`);
 }
+
+// [GET]   /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        let find = {
+            _id: id,
+            deleted: false
+        }
+
+        const data = await Role.findOne(find);
+        console.log(data.id);
+        
+        res.render("admin/pages/roles/edit.pug", {
+            pageTitle: "Sửa nhóm quyền",
+            data: data
+        });
+    } catch (error) {
+        res.redirect(`${systemConfig.preFixAdmin}/roles`)
+    }
+};
+
+// [PATCH]   /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        await Role.updateOne({_id: id}, req.body);
+
+        req.flash("success", "Cập nhật thành công");
+
+    } catch (error) {
+        req.flash("success", "Cập nhật thất bại");
+    }
+    res.redirect("back");
+};
