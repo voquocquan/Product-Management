5331bd9e8b237639b66a46058f562d0a5e3178be
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-09 09:21:20 +0700

    "sửa" danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,7 +1,8 @@
 const ProductCategory = require("../../models/product-category.model")
 const systemConfig = require("../../config/system")
 
-const createTreeHelper = require("../../helpers/createTree")
+const createTreeHelper = require("../../helpers/createTree");
+const { request } = require("express");
 
 // [GET]   /admin/products-category
 module.exports.index = async (req, res) => {
@@ -49,3 +50,38 @@ module.exports.createPost = async (req, res) => {
 
     res.redirect(`${systemConfig.preFixAdmin}/products-category`);
 };
+
+// [GET]   /admin/products-category/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await ProductCategory.findOne({
+            _id: id,
+            deleted: false
+        });
+        const records = await ProductCategory.find({
+            deleted: false,
+        });
+    
+        const newRecords = createTreeHelper.tree(records);
+    
+        res.render("admin/pages/products-category/edit", {
+            pageTitle: "Chỉnh sửa danh mục sản phẩm",
+            data: data,
+            records: newRecords,
+        });
+    } catch (error) {
+        res.redirect(`${systemConfig.preFixAdmin}/products-category`)
+    }
+}
+
+// [GET]   /admin/products-category/edit/:id
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+
+    req.body.position = parseInt(req.body.position)
+
+    await ProductCategory.updateOne({_id: id}, req.body)
+
+    res.redirect("back")
+}   
--- routers/admin/product-category.route.js
+++ routers/admin/product-category.route.js
@@ -20,5 +20,12 @@ router.post(
     controller.createPost
 );
 
+router.get("/edit/:id", controller.edit);
+
+router.patch("/edit/:id",
+    upload.single('thumbnail'),
+    uploadCloud.upload,
+    validate.createPost,
+    controller.editPatch);
 
 module.exports = router;
