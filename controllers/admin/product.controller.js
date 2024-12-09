6653937893f02b3f3eaa22f9a0106bc3fe038cdc
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-09 09:45:29 +0700

    thêm mới sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model")
+const ProductCategory = require("../../models/product-category.model")
 
 const systemConfig = require("../../config/system")
 
@@ -8,6 +9,7 @@ const searchHelper = require("../../helpers/search")
 
 const paginationHelper = require("../../helpers/pagination")
 
+const createTreeHelper = require("../../helpers/createTree");
 
 // [GET]   /admin/products
 module.exports.index = async (req, res) => {
@@ -156,8 +158,17 @@ module.exports.deleteItem = async (req, res) => {
 
 // [GET]   /admin/products/create
 module.exports.create = async (req, res) => {
+    let find = {
+        deleted: false,
+    };
+
+    const category = await ProductCategory.find(find)
+
+    const newCategory = createTreeHelper.tree(category);
+
     res.render("admin/pages/products/create", {
         pageTitle: "Thêm mới sản phẩm",
+        category: newCategory
     });
 };
 
