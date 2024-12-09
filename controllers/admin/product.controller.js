89ecf63f8a36d1c67fea2ae5268498602e4299c1
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-09 09:58:06 +0700

    sửa sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -202,9 +202,16 @@ module.exports.edit = async (req, res) => {
 
         const product = await Product.findOne(find);
 
+        const category = await ProductCategory.find({
+            deleted: false,
+        })
+
+        const newCategory = createTreeHelper.tree(category);
+
         res.render("admin/pages/products/edit", {
             pageTitle: "Chỉnh sửa sản phẩm",
-            product: product
+            product: product,
+            category: newCategory
         });
     } catch (error) {
         // req.flash("error", `Không tìm thấy trang!`);
