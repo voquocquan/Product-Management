112b9b83323d1d5de09e0f4f390a74267ae49744
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-12 01:38:33 +0700

    thêm authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,17 +1,24 @@
-const Account = require("../../models/account.model")
+const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 
 const systemConfig = require("../../config/system");
 
 module.exports.requireAuth = async (req, res, next) => {
-    
-    if(!req.cookies.token) {
+
+    if (!req.cookies.token) {
         res.redirect(`${systemConfig.preFixAdmin}/auth/login`);
     } else {
 
-        const user = await Account.findOne({ token: req.cookies.token});
-        if(!user) {
+        const user = await Account.findOne({ token: req.cookies.token }).select("-password");
+        if (!user) {
             res.redirect(`${systemConfig.preFixAdmin}/auth/login`);
-        }else{
+        } else {
+            const role = await Role.findOne({
+                _id: user.role_id
+            }).select("title permissions")
+
+            res.locals.user = user;
+            res.locals.role = role;
             next();
         }
     }
