57f433b7234a7729c395915cddcb89fd25c953db
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-14 01:28:42 +0700

    lấy ra danh sách sản phẩm client

--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,5 +1,8 @@
 const Product = require("../../models/product.model")
+const ProductCategory = require("../../models/product-category.model")
+
 const productsHelper = require("../../helpers/product")
+const productsCategoryHelper = require("../../helpers/product-category")
 
 
 // [GET]  /products
@@ -36,4 +39,29 @@ module.exports.detail = async (req, res) => {
         // req.flash("error", `Không tìm thấy trang!`);
         res.redirect(`/products`);
     }
+};
+
+// [GET]  /products/:slugCategory
+module.exports.category = async (req, res) => {
+    
+    const category = await ProductCategory.findOne({
+        slug: req.params.slugCategory,
+        deleted: false
+    });
+
+    const listSubCategory = await productsCategoryHelper.getSubCategory(category.id);
+
+    const listSubCategoryId = listSubCategory.map(item => item.id);
+
+    const products = await Product.find({
+        product_category_id: { $in: [category.id, ...listSubCategoryId]},
+        deleted: false
+    }).sort({ position: "desc"});
+
+    const newProducts = productsHelper.priceNewProduct(products);
+    
+    res.render("client/pages/products/index.pug",{
+        pageTitle: category.title,
+        products: newProducts
+    });
 };
--- routers/client/products.route.js
+++ routers/client/products.route.js
@@ -4,6 +4,9 @@ const controller = require("../../controllers/client/products.controller")
 
 router.get("/", controller.index)
 
-router.get("/:slug", controller.detail)
+router.get("/:slugCategory", controller.category)
+
+
+// router.get("/:slug", controller.detail)
 
 module.exports = router;
