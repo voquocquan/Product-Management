3c6269c80abdfef19e7bf0b820da34ab8be51936
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-18 04:32:15 +0700

    giao diện trang đặt hàng thành công

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -2,7 +2,6 @@ const Cart = require("../../models/cart.model")
 const Product = require("../../models/product.model")
 const Order = require("../../models/order.model")
 
-
 const productHelper = require("../../helpers/product")
 
 //[GET] /checkout/
@@ -87,9 +86,26 @@ module.exports.order = async (req, res) => {
 
 //[GET] /checkout/success/:orderId
 module.exports.success = async (req, res) => {
-  console.log(req.params.orderId);
-  
+  const order = await Order.findOne({
+    _id: req.params.orderId
+  });
+
+  for (const product of order.products) {
+    const productInfo = await Product.findOne({
+      _id: product.product_id
+    }).select(" title thumbnail  ");
+
+    product.productInfo = productInfo;
+    product.priceNew = productHelper.priceNewProducts(product);
+
+    product.totalPrice = product.priceNew * product.quantity;
+    
+  };
+
+  order.totalPrice = order.products.reduce((sum, item) => sum + item.totalPrice, 0)
+
   res.render("client/pages/checkout/success", {
     pageTitle: "Đặt hàng thành công",
+    order: order
   });
 };
