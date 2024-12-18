195f9f39520b9bdd620308d0186c7d7d951cf7c8
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-18 17:31:34 +0700

    đăng xuất

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -63,3 +63,9 @@ module.exports.loginPost = async (req, res) => {
   res.redirect("/")
 };
 
+// [GET]  /user/logout
+module.exports.logout = async (req, res) => {
+  res.clearCookie("tokenUser");
+  res.redirect("/")
+};
+
--- routers/client/user.route.js
+++ routers/client/user.route.js
@@ -13,6 +13,7 @@ router.get('/login', controller.login)
 
 router.post('/login', validate.loginPost, controller.loginPost)
 
+router.get('/logout', controller.logout)
 
 
 module.exports = router;
