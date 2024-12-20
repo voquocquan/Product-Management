17776375c3286efaf49a45bf60fefbe7b659625e
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-20 07:36:05 +0700

    trang tổng quan bên admin

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,8 +1,85 @@
+const ProductCategory = require("../../models/product-category.model")
+const Product = require("../../models/product.model")
+const Account = require("../../models/account.model")
+const User = require("../../models/user.model")
+
 // [GET]   /admin/dashboard
+module.exports.dashboard = async (req, res) => { 
+    const statistic = {
+        categoryProduct: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        product: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        account: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        user: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+    }
+    //Danh mục sản phẩm
+    statistic.categoryProduct.total = await ProductCategory.countDocuments({
+        deleted: false
+    });
+    statistic.categoryProduct.active = await ProductCategory.countDocuments({
+        status: "active",
+        deleted: false
+    });
+    statistic.categoryProduct.inactive = await ProductCategory.countDocuments({
+        status: "inactive",
+        deleted: false
+    });
 
+    //Sản phẩm
+    statistic.product.total = await Product.countDocuments({
+        deleted: false
+    });
+    statistic.product.active = await Product.countDocuments({
+        status: "active",
+        deleted: false
+    });
+    statistic.product.inactive = await Product.countDocuments({
+        status: "inactive",
+        deleted: false
+    });
+    //Tài khoản admin
+    statistic.account.total = await Account.countDocuments({
+        deleted: false
+    });
+    statistic.account.active = await Account.countDocuments({
+        status: "active",
+        deleted: false
+    });
+    statistic.account.inactive = await Account.countDocuments({
+        status: "inactive",
+        deleted: false
+    });
+    
+    //Tài khoản client
+    statistic.user.total = await User.countDocuments({
+        deleted: false
+    });
+    statistic.user.active = await User.countDocuments({
+        status: "active",
+        deleted: false
+    });
+    statistic.user.inactive = await User.countDocuments({
+        status: "inactive",
+        deleted: false
+    });
 
-module.exports.dashboard = (req, res) => { 
     res.render("admin/pages/dashboard/index.pug", {
-        pageTitle: "Trang tổng quan"
+        pageTitle: "Trang tổng quan",
+        statistic: statistic
     });
 }
